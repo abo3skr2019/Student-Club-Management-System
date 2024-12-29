b7679f63c88387957f3191c532f423902da081c1
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2024-12-29 17:33:16 +0300

    feat(backend): add User model with schema for user data management

---
+++ backend/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ClubSchema = require('./club').schema; // Import ClubSchema
+
+const ProviderSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    providerId: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+const UserSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true
+    },
+    displayName: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    profileImage: {
+        type: String,
+        required: true
+    },
+    ClubsManaged: [ClubSchema],
+    providers: [ProviderSchema],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
