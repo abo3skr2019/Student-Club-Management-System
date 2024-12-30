0caf49cd45c9da381ce71a2e3b55ce86969739ca
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2024-12-30 11:27:08 +0300

    feat(profile): integrate profile routes into main application

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ app.use(express.static(path.join(__dirname, 'frontend', 'public')));
 // Routes for EJS templates
 app.get('/', (req, res) => res.render('index'));
 app.get('/login', (req, res) => res.render('login'));
-app.get('/profile', (req, res) => res.render('profile'));
 app.get('/club-dashboard', (req, res) => res.render('club-dashboard'));
 app.get('/event-creation', (req, res) => res.render('event-creation'));
 app.get('/feed', (req, res) => res.render('feed'));
 app.get('/event-admin-view', (req, res) => res.render('event-admin-view'));
 app.get('/event-user-view', (req, res) => res.render('event-user-view'));
 
+app.use('/',require('./routes/profile'));
 // Start the server
 const PORT = 3000;
 app.listen(PORT, () => {
