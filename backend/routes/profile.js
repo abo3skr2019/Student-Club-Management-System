1cd5330eb4e6284b1f293855249920fcff1f0be5
AF_Askar <68832286+abo3skr2019@users.noreply.github.com>
2024-12-30 04:13:53 +0300

    feat(profile): add authentication middleware to profile routes

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { isAuthenticated } = require('../middleware/CheckAuth');
+
 
 // Render the profile update form
-router.get('/update-profile', (req, res) => {
+router.get('/update-profile',isAuthenticated, (req, res) => {
     res.render('update-profile', { user: req.user });
 });
 
 // Handle the profile update form submission
-router.post('/update-profile', async (req, res) => {
+router.post('/update-profile',isAuthenticated, async (req, res) => {
 
     const { firstName, lastName } = req.body;
     try {
@@ -23,11 +25,10 @@ router.post('/update-profile', async (req, res) => {
     }
 });
 
-
-// render User profile
-router.get('/profile/:id', async (req, res) => {
+// Render the logged-in user's profile
+router.get('/profile', isAuthenticated, async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
+        const user = await User.findById(req.user.id);
         if (!user) {
             return res.status(404).send('User not found');
         }
@@ -36,4 +37,5 @@ router.get('/profile/:id', async (req, res) => {
         res.status(500).send('Server error');
     }
 });
+
 module.exports = router;
