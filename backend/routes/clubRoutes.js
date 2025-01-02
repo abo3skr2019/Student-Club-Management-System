339b943b359ca1e533cd3238cc2a79349d8a7bc1
Fkhrayef <alkhrayefff@gmail.com>
2025-01-02 03:13:39 +0300

    Fix: deleted isAdmin from put is it's not needed.

--- backend/routes/clubRoutes.js
+++ backend/routes/clubRoutes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 const clubController = require('../controllers/clubController');
 const { isAuthenticated } = require('../middleware/CheckAuth');
+const { isAdmin, isClubAdmin } = require('../middleware/CheckRoles');
 
 // Public Routes
 router.get('/', clubController.getAllClubs);
 router.get('/:clubId', clubController.getClubById);
 
 // Protected routes - require authentication
-router.post('/', isAuthenticated, clubController.createClub);
-router.put('/:clubId', isAuthenticated, clubController.updateClub);
+router.post('/', isAdmin, isAuthenticated, clubController.createClub);
+router.put('/:clubId', isClubAdmin, isAuthenticated, clubController.updateClub);
 router.post('/:clubId/assign-admin', isAuthenticated, clubController.assignClubAdmin);
 
 module.exports = router;
