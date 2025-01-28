516e5d67c983ea5a600f2552b5fe2b1dd8f4aa90
Abdulaziz AlFrayan <abdulaziizbinkhaled@gmail.com>
2025-01-29 00:03:13 +0300

    removed the unneccessary changes on app.js

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ connectDB();
 app.get('/', (req, res) => res.render('index'));
 app.get('/login', (req, res) => res.render('login'));
 app.get('/club-dashboard', (req, res) => res.render('club-dashboard'));
-app.get('/event-creation', (req, res) => res.render('event-creation', { currentPage: 'event-creation' }));
+app.get('/event-creation', (req, res) => res.render('event-creation'));
 app.get('/feed', (req, res) => res.render('feed'));
 app.get('/event-admin-view', (req, res) => res.render('event-admin-view'));
 app.get('/event-user-view', (req, res) => res.render('event-user-view'));
