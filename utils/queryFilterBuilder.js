80d53a30f7bb70c28ff6f17852ad3f7ea29ffa19
Fkhrayef <alkhrayefff@gmail.com>
2025-05-09 15:20:09 +0300

    Add utility functions for building database query filters and selecting fields

---
+++ utils/queryFilterBuilder.js
@@ -0,0 +1,59 @@
+/**
+ * Utilities for building DB queries from parsed request filters
+ */
+const { eq, neq, gt, gte, lt, lte } = require('drizzle-orm');
+
+/**
+ * Translate parsedQuery.filters into Drizzle conditions
+ * @param {Object} filters - Object of the format { field: { op: value, ... }, ... }
+ * @param {Object} model - Drizzle schema model (e.g., club, clubMembership)
+ * @returns {Array} Array of Drizzle condition expressions
+ */
+function buildFilterConditions(filters = {}, model) {
+    const conditions = [];
+
+    for (const [field, ops] of Object.entries(filters)) {
+        if (!model[field]) continue; // Skip if field doesn't exist on model
+
+        for (const [op, value] of Object.entries(ops)) {
+            switch (op) {
+                case 'eq':
+                    conditions.push(eq(model[field], value));
+                    break;
+                case 'neq':
+                    conditions.push(neq(model[field], value));
+                    break;
+                case 'gt':
+                    conditions.push(gt(model[field], value));
+                    break;
+                case 'gte':
+                    conditions.push(gte(model[field], value));
+                    break;
+                case 'lt':
+                    conditions.push(lt(model[field], value));
+                    break;
+                case 'lte':
+                    conditions.push(lte(model[field], value));
+                    break;
+                case 'in':
+                    conditions.push(
+                        model[field].in(Array.isArray(value) ? value : [value]),
+                    );
+                    break;
+                case 'nin':
+                    conditions.push(
+                        model[field].notIn(
+                            Array.isArray(value) ? value : [value],
+                        ),
+                    );
+                    break;
+            }
+        }
+    }
+
+    return conditions;
+}
+
+module.exports = {
+    buildFilterConditions,
+};
